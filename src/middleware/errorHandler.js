6d81577aec57e75f54e3b8f2f9e4490cf18f6bb7
Daniil Rybchynskyi <rybchynskyidaniil@gmail.com>
2026-02-25 21:25:52 +0000

    ff

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,10 +1,16 @@
 
+import createHttpError from "http-errors";
+
 export const errorHandler = (err, req, res, next) => {
   console.error(err);
 
   const isProd = process.env.NODE_ENV === "production";
-
-  res.status(500).json({
+  if (err instanceof createHttpError.HttpError) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
+  return res.status(500).json({
     message: isProd
       ? "Something went wrong. Please try again later."
       : err.message,
--- src/models/note.js
+++ src/models/note.js
@@ -10,7 +10,7 @@ const noteSchema = new Schema(
     },
     content: {
       type: String,
-      required: true,
+      required: false,
       trim: true,
       default:"",
     },
